119b854d2e59b7b5ce5c236ac2e9aa688ab5f075
halle <halle.mills.2022@student.lhschools.org>
2019-10-17 09:28:36 -0400

    made my methods

---
+++ src/org/LickingHeights/Main.java
@@ -0,0 +1,20 @@
+package org.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        printGrettings();
+
+
+
+    }
+
+
+    public static void printGrettings(){
+        System.out.println("Hello World");
+        System.out.println("Hello Friends");
+        System.out.println("Hello Universe");
+        System.out.println("Hello Stars");
+    }
+}
